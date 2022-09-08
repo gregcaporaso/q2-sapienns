abc56985761d6d377b00062d89ff99cf9ed01497
Greg Caporaso <greg.caporaso@nau.edu>
2022-09-08 09:58:33 -0700

    changes to pass ci (#3)
    * add copyright headers
    * update formatting of LICENSE

--- q2_sapienns/tests/test_plugin_setup.py
+++ q2_sapienns/tests/test_plugin_setup.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2022, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 import unittest
 
 from q2_sapienns.plugin_setup import plugin as sapienns_plugin
